7fcc4277f9270a5cfe1c12257eecfeaf99419e90
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2025-01-20 08:56:08 +0000

    Gmae moves and initialized with the class

---
+++ TikTakToeGame/tiktaktoe.h
@@ -0,0 +1,122 @@
+#ifndef TIKTAKTOE_H
+#define TIKTAKTOE_H
+
+#include "board.h"
+#include "player.h"
+#include "playingPiece.h"
+#include <deque>
+#include <string>
+using namespace std;
+
+class TikTakToe {
+
+private:
+public:
+  deque<Player *> players;
+  Board *gameBoard;
+  TikTakToe() { initializeGame(); }
+  void initializeGame() {
+    gameBoard = new Board(3);
+    PlayingPiece crossPiece = PlayingPieceX();
+    PlayingPiece circlePiece = PlayingPieceO();
+    Player *player1 = new Player("Player 1", crossPiece);
+    Player *player2 = new Player("Player 2", circlePiece);
+    players.push_back(player1);
+    players.push_back(player2);
+  }
+
+  string startGame() {
+    // bool noWiner = false;
+    bool winner = false;
+    Player *player;
+    // gameBoard->printBoard();
+    while (!winner) {
+
+      player = players.front();
+      players.pop_front();
+
+      gameBoard->printBoard();
+
+      // check for the free space
+
+      vector<pair<int, int>> freecells = gameBoard->getFreeCells();
+      if (freecells.size() == 0) {
+        winner = false;
+        break;
+      }
+
+      cout << "It's " << player->getName() << " turn" << endl;
+      cout << "Enter row and column" << endl;
+      int row, col;
+      cout<< "Hello";
+      cin >> row >> col;
+      cout << row << " " << col;
+      PlayingPiece *temp = nullptr;
+      *temp = player->getPiece();
+      bool val = gameBoard->setPiece(row, col, temp);
+      if (val == false) {
+        cout << "Invalid move" << endl;
+        players.push_front(player);
+        continue;
+      }
+      // check for the winner
+
+      players.push_back(player);
+      *temp = player->getPiece();
+      if (winnerCheck(row, col, temp)) {
+        winner = true;
+        return player->getName();
+      }
+      // continue;
+    }
+    return "Tie";
+  }
+
+  bool winnerCheck(int row, int col, PlayingPiece *piece) {
+
+    int count = 0;
+    int size = gameBoard->getSize();
+    // check for the row
+    for (int i = 0; i < size; i++) {
+      if (gameBoard->getPiece(row, i).getType() == piece->getType()) {
+        count++;
+      }
+    }
+    if (count == size) {
+      return true;
+    }
+    // check for the column
+    count = 0;
+    for (int i = 0; i < size; i++) {
+      if (gameBoard->getPiece(i, col).getType() == piece->getType()) {
+        count++;
+      }
+    }
+    if (count == size) {
+      return true;
+    }
+    // check for the diagonal
+    count = 0;
+    for (int i = 0; i < size; i++) {
+      if (gameBoard->getPiece(i, i).getType() == piece->getType()) {
+        count++;
+      }
+    }
+    if (count == size) {
+      return true;
+    }
+
+    // check for the diagonal
+    count = 0;
+    for (int i = 0; i < size; i++) {
+      if (gameBoard->getPiece(i, size - 1 - i).getType() == piece->getType()) {
+      }
+    }
+    if (count == size) {
+      return true;
+    }
+    return false;
+  }
+};
+
+#endif // BOARDS_H
