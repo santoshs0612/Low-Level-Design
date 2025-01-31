32e1fcac3b18e6c55c02b98279e876ae71889b8e
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2025-01-31 10:20:34 +0000

    Coins Added for the purchase

---
+++ VendingMachine LLD/Coins.h
@@ -0,0 +1,19 @@
+
+#ifndef COINS_H
+#define COINS_H
+#include <iostream>
+#include <vector>
+enum Coins { PENNY = 1, NICKEL = 5, DIME = 10, QUARTER = 25 };
+// class Coins {
+// public:
+//   enum coins { PENNY = 1, NICKEL = 5, DIME = 10, QUARTER = 25 };
+//   // private:
+//   //   std::vector<int> coin;
+
+// public:
+//   int value;
+//   Coins(int value) { this->value = value; }
+//   // void addCoin(int coin) { coin.push_back(coin); }
+// };
+
+#endif
